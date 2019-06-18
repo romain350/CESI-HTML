cc3af57017209021553339496532a8af7401c09f
Hesses <romainwanner1998@gmail.com>
2019-06-18 09:47:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,23 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1>MON CV</h1>
+	
+	<h2>Généralités</h2>
+	<h3>Romain WANNER</h3>
+	<p>Informaticien</p>
+	<p>20 ans, Obernai</p>
+	<p>romainwanner1998@gmail.com</p>
+	
+	<h2>Compétences</h2>
+	<h3>Administration systèmes et réseaux</h3>
+	<h3>Autres</h3>
+	
+	<h2>Expériences professionnelles</h2>
+	
+	<h2>Fromations</h2>
+	
+	<h2>Centre d'intérêts</h2>
 
     <!-- Optional JavaScript -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
