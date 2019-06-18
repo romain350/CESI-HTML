2646e3e7a10170fbd770e5d4b19cc14c530cc4c6
Hesses <romainwanner1998@gmail.com>
2019-06-18 10:17:35 +0200

    update

---
+++ CV3/index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>MON CV -Etape 2 : Ajout des listes</h1>
+	<img src="./jul.jpg" alt="Le titre alternatif décrivant l'image si celle si ne s'affiche pas">
+		<h2>Généralités</h2>
+			<p><b>Romain WANNER</b></br>
+			Informaticien</br>
+			20 ans, Obernai</br>
+			romainwanner1998@gmail.com</br>
+			<a href="https://fr.linkedin.com/">LINKEDIN</a></p>
+		<h2>Compétences</h2>
+			<h3>Administration systèmes et réseaux</h3>
+				<ul>
+					<li>Administration Windows serveur</li>
+					<li>Administration web</li>
+					<li>OS clients: Windows 10, 7 </li>
+					<li>Virtualisation VMWARE ESXI, Proxmox</li>
+					<li>Scripting</li>
+				</ul>
+			<h3>Exploitation</h3>
+				<ul>
+					<li>Exploitation</li>
+					<li>Support technique</li>
+					<li>support technique utilisateur</li>
+					<li>Formation des utilisateurs</li>
+				</ul>
+		<h2>Expériences professionnelles</h2>
+				<ol>
+					<li>2016 - 2016 2CRSI</li>
+					<li>2017 - 2019 MFA</li>
+					<li>2016 - 2016 Visual média</li>
+				</ol>
+		<h2>Fromations</h2>
+			<ol>
+				<li>2016 - 2016 2CRSI</li>
+				<li>2017 - 2019 MFA</li>
+				<li>2016 - 2016 Visual média</li>
+			</ol>
+		<h2>Centre d'intérêts</h2>
+			<ul>
+				<li>J'ai pas d eloisir</li>
+			</ul>
+			
+		<h2>Carte</h2>
+		<div style="width: 100%"><iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=41%20rue%20othon-pisot%20+(Chez%20moi)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/map-my-route/">Create route map</a></iframe></div><br />
+    <!-- Optional JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
+  </body>
+</html>
