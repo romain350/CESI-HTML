8b946e4aa0d4af31f5dcede992bb44d2cb9d8f99
Retard Wizard <romainwanner1998@gmail.com>
2019-07-01 15:05:10 +0200

    update

---
+++ CVBOOTSTRAP/index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./kernel/css/bootstrap.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+
+    <title>cv</title>
+  </head>
+  <body style="background-color: lightgrey;">
+<div style="background-color: white;width: 80%;margin: 10px auto auto;padding: 50px 50px 50px 50px;">	<div  class="container">
+		<div style="background-color: lightblue;" class="row">
+			<div class="col-10 " style="text-align: center;margin: 25px 0px 10px;"><h1 style="
+">MON CV</h1></div>
+			<div class="col-2 ">
+			<img style="width: 200px;float: right;" src="./cesi.png">
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-xl-2 col-4"><img style="height: 150px;" src="./jul.jpg"></div>
+			<div class="col-xl-6 col-8">				<p><font color="darkblue"><b>Romain WANNER</b></font></br>
+				<font color="red">Informaticien</font></br>
+				20 ans, Obernai</br>
+				<i class="fas fa-envelope"></i> romainwanner1998@gmail.com</br>
+				<i class="fab fa-linkedin"></i> <a href="https://fr.linkedin.com/">LINKEDIN</a></p>
+			</div>
+			<div class="col-xl-2 col-12 ">
+			<center>
+				<iframe width="auto" height="150" src="https://maps.google.com/maps?width=100%&amp;height=9&amp;hl=en&amp;q=41%20rue%20othon-pisot+(Chez%20tonton)&amp;ie=UTF8&amp;t=&amp;z=16&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+			</center>
+			</div>
+		</div>
+		
+		
+		<br>
+		
+		
+		<div class="row">
+			<div class="col-xl-6">
+				<h5><p style="background-color: lightblue; box-shadow: 8px 10px 0px #aaa;"><i class="fas fa-briefcase"></i> Exploitation</p></h5>
+			<ul>
+				<li>Exploitation</li>
+				<li>Support technique</li>
+				<li>support technique utilisateur</li>
+				<li>Formation des utilisateurs</li>
+			</ul>
+			</div>
+			<div class="col-xl-6">
+				<h5><p style="background-color: lightblue; box-shadow: 8px 10px 0px #aaa;"><i class="fas fa-briefcase"></i> Compétences</p></h5>
+				<ul>
+					<li><b>Administration</b> Windows serveur</li>
+					<li><b>Administration</b> web</li>
+					<li><b>OS clients</b> Windows 10, 7 </li>
+					<li><b>Virtualisation</b> VMWARE ESXI, Proxmox</li>
+					<li><b>Scripting</b></li>
+				</ul>
+			</div>
+		</div>
+		
+		
+		<div class="row">
+			<div class="col-xl-6">
+				<h5><p style="background-color: lightblue; box-shadow: 8px 10px 0px #aaa;"><i class="fas fa-building"></i> Expériences professionnelles</p></h5>
+				<ol>
+					<li>2016 - 2016 2CRSI</li>
+					<li>2017 - 2019 MFA</li>
+					<li>2016 - 2016 Visual média</li>
+				</ol>
+			</div>
+			<div class="col-xl-6">
+				<h5><p style="background-color: lightblue; box-shadow: 8px 10px 0px #aaa;"><i class="fas fa-graduation-cap"></i> Fromations</p></h5>
+			<ol>
+				<li>2016 - 2016 2CRSI</li>
+				<li>2017 - 2019 MFA</li>
+				<li>2016 - 2016 Visual média</li>
+			</ol>
+			</div>
+		</div>
+		
+		
+		<div class="row">
+			<div class="col-xl-6">
+				<h5><p style="background-color: lightblue; box-shadow: 8px 10px 0px #aaa;"><i class="fas fa-beer"></i> Fromations</p></h5>
+				<ol>
+					<li>2016 - 2016 2CRSI</li>
+					<li>2017 - 2019 MFA</li>
+					<li>2016 - 2016 Visual média</li>
+				</ol>
+			</div>
+			<div class="col-xl-6">
+				<h5><p style="background-color: lightblue; box-shadow: 8px 10px 0px #aaa;"><i class="fas fa-globe-europe"></i> Expériences professionnelles</p></h5>
+				<ol>
+					<li>2016 - 2016 2CRSI</li>
+					<li>2017 - 2019 MFA</li>
+					<li>2016 - 2016 Visual média</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="./kernel/js/jquery-3.3.1.js"></script>
+    <script src="./kernel/js/popper.js"></script>
+    <script src="./kernel/js/bootstrap.js"></script>
+	</div>
+  </body>
+</html>
