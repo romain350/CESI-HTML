21643317d721d3f35f84d81ae09326ff6fca024e
Retard Wizard <romainwanner1998@gmail.com>
2019-07-01 15:05:43 +0200

    Create index.php

---
+++ BOOTSTRAP/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./kernel/css/bootstrap.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <style>
+	div {
+		border: solid 1px ;
+	}
+  </style>
+	<div class="container">
+		<h1>Hello, world!</h1>
+		<div class="row">
+			<div class="col-xl-6">
+			<br>
+				<div class="row">
+					<div class="col-xl-4">largeur 4</div>
+					<div class="col-xl-4 offset-4">largeur 4</div>
+				</div>
+			</div>
+			<div class="col-xl-6">
+			
+				<div class="row">
+					<div class="col-xl-4">largeur 4</div>
+					<div class="col-xl-8">largeur 4</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="./kernel/js/jquery-3.3.1.js"></script>
+    <script src="./kernel/js/popper.js"></script>
+    <script src="./kernel/js/bootstrap.js"></script>
+  </body>
+</html>
